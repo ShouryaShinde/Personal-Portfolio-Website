b7487d1be71f8031dea25597951b9f65a71a5a2b
Shourya Shinde <shindeshauryaanil@gmail.com>
2026-05-03 16:13:35 +0530

    Fix : Adjust page width

--- frontend/src/assets/SectionHeader.jsx
+++ frontend/src/assets/SectionHeader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function SectionHeader(props) {
   return (
-    <div className="flex items-center justify-center mt-10 w-11/12 mx-auto gap-5">
+    <div className="flex items-center justify-center mt-10 w-full mx-auto gap-5">
       <h1 className="text-3xl font-bold ml-7 whitespace-nowrap space-grotesk">
         {props.title}
       </h1>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,14 +7,16 @@ import Techstack from "../components/Techstack.jsx" ;
 
 function Home() {
   return (
-    <div>
-      <Hero />
-      <SectionHeader title="Career Highlights" />
-      <Highlights />
-      <SectionHeader title="Tech Stack" />
-      <Techstack />
-      <SectionHeader title="Competitive Programming Stats"/>
-      <CPStats />
+    <div className="flex flex-col items-center w-full">
+      <div className="w-11/12">
+        <Hero />
+        <SectionHeader title="Career Highlights" />
+        <Highlights />
+        <SectionHeader title="Tech Stack" />
+        <Techstack />
+        <SectionHeader title="Competitive Programming Stats"/>
+        <CPStats />
+      </div>
     </div>
   );
 }
